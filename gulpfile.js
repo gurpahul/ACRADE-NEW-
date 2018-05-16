e3e9860d3ed6707c0cfff7560ccd8d1b25c477bc
GURPAHUL-SINGH <gurpahulsingh13@gmail.com>
2018-05-16 15:41:23 -0500

    Autoprfixer is added by gulp to Acrade

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('default', function(){
+
+});
+
+gulp.task('css', function(){
+    gulp.src('./src/css/*.css')
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('./dist/css/'))
+})
