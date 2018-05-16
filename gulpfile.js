f2056714e5b1086f2cf862336c7a6127eb8378d5
GURPAHUL-SINGH <gurpahulsingh13@gmail.com>
2018-05-16 16:05:37 -0500

    clean -- css added by gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
+let cleanCSS = require('gulp-clean-css');
 
 gulp.task('default', function(){
 
@@ -11,5 +12,8 @@ gulp.task('css', function(){
         browsers: ['last 2 versions'],
         cascade: false
     }))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    
     .pipe(gulp.dest('./dist/css/'))
+
 })
